cc4ec3c53e1d0eb9f5a5004905704e4511a6da83
Hansith <hansithlochana@gmail.com>
2024-09-10 09:38:01 +0530

    first commit

---
+++ Library.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Library {
+    private List<Book> books;
+    private List<Member> members;
+
+    public Library() {
+        books = new ArrayList<>();
+        members = new ArrayList<>();
+    }
+}
