1c018e1637b2f0729a8d1ac620955deaed565326
Hansith <hansithlochana@gmail.com>
2024-09-10 09:52:56 +0530

    Added Member.java file

---
+++ Member.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Member {
+    private int id;
+    private String name;
+    private List<Book> borrowedBooks;
+
+    public Member(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public String getInfo() {
+        return "ID: " + id + ", Name: " + name;
+    }
+}
